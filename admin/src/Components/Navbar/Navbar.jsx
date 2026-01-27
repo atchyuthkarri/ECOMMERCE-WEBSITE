5728b1696316faf809497a8be622410920b41ea9
atchyuthkarri <atchyuthkarri46@gmail.com>
2026-01-27 23:04:27 +0530

    updated

--- admin/src/Components/Navbar/Navbar.jsx
+++ admin/src/Components/Navbar/Navbar.jsx
@@ -6,7 +6,7 @@ import navProfile from "../../assets/nav-profile.svg"
 const Navbar = () => {
   return (
     <div className='navbar'>
-        <img src={navlogo} alt='' className='nav-logo' />
+        <p>Storely</p>
         <img src={navProfile} alt='' className='nav-profile'/>
     </div>
   )
